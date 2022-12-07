2a7862fde0420e2ff62241f41644b14e021ca5bd
Joel Da Rosa Junior <joeldarosajunior@gmail.com>
2022-12-07 15:24:04 -0300

    Update data.py

--- data.py
+++ data.py
@@ -6,6 +6,9 @@ class Datas:
     def __init__(self) -> None:
         self.momento_cadastro = datetime.today()
         
+    def __str__(self) -> str:
+        return self.format()
+        
     def mes(self):
         meses = ('janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro')
         mes_cadastro = self.momento_cadastro.month -1
@@ -17,3 +20,12 @@ class Datas:
         )
         dia_semana = self.momento_cadastro.weekday() 
         return semana[dia_semana]
+    
+    def format(self):
+        data_formatada = self.momento_cadastro.strftime(r'%d/%m/%Y %H:%M')
+        return data_formatada
+    
+    def tempo_cadastro(self):
+        tempo = datetime.today() - self.momento_cadastro
+        return tempo
+    
